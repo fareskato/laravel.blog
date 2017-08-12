aeb72898bc7ff6b115044264e0bc25e05a8a7feb
fares kato <fareskato@gmail.com>
2017-08-12 14:05:34 +0300

    display all posts part 2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,8 +7,15 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    // posts per page
+    protected $limit = 3;
+
     public function index(){
-        $posts = Post::all();
-        return view('blog.index', compact('posts', $posts));
+
+
+//        $posts = Post::all();
+        // This way best performance
+        $posts = Post::with('author')->simplePaginate($this->limit); // or paginate(3)
+         return view('blog.index', compact('posts', $posts));
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
+//  Relationship with author(user model)
+    public function author(){
+        return $this->belongsTo(User::class);
+    }
+
     // image usr accessor
     public function getImageUrlAttribute(){
 //        $imageUrl = "http://via.placeholder.com/750x450";
@@ -18,6 +24,12 @@ class Post extends Model
         }
         return $imageUrl;
     }
+
+    // date accessor
+    public function getDateAttribute(){
+        return $this->created_at->diffForHumans();
+    }
+
 
 
 }
